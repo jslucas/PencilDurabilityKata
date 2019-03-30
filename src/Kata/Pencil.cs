987fd730d2f8fb646a19b06eadb80d861f588fbd
jslucas <jspencerlucas@gmail.com>
2019-03-30 18:34:49 -0400

    Added WhenAPencilIsSharpenedItsDurabilityIsResetToInitialValue test

--- src/Kata/Pencil.cs
+++ src/Kata/Pencil.cs
@@ -5,15 +5,24 @@ namespace Kata
     public class Pencil
     {
         public string Text { get; set; }
+        private int initialDurability;
         public int Durability { get; set; } = 0;
 
         public Pencil() { }
 
+
         public Pencil(int durability)
         {
+            this.initialDurability = durability;
             this.Durability = durability;
         }
 
-    }
 
+        public void Sharpen()
+        {
+            this.Durability = initialDurability;
+        }
+
+
+    }
 }
